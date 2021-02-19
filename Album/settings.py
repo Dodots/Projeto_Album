d8558a0272023f19400f38318d0a2821aab2a600
Douglas <doouglascarvalho92@gmail.com>
2021-02-19 12:11:04 -0300

    removendo o # Configure Django App for Heroku.

--- Album/settings.py
+++ Album/settings.py
@@ -132,24 +132,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-
-
-
-
 # Configuração de autenticação
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
 
 
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
